3e0531c9a78c18ccda50d7e4fa0fbdd893f5c990
codionteam <codion2025@gmail.com>
2025-11-14 00:59:30 +0600

    Add files via upload

---
+++ Lab 1/Lab1_1_Print_a_String.c
@@ -0,0 +1,16 @@
+// #include<stdio.h>
+// int main()
+// {
+//     char a[] = "Hi Codion";
+//     printf("%s", a);
+//     return 0;
+// }
+
+#include<stdio.h>
+int main()
+{
+    char a[100];
+    gets(a);
+    printf("%s", a);
+    return 0;
+}
